684b1058ea689cd107fa6f64e082394e20115679
Sarah <sarahahmad17@gmail.com>
2019-04-19 14:34:58 -0400

    removed commented line

--- src/App.js
+++ src/App.js
@@ -19,7 +19,6 @@ class App extends React.Component {
   getWeather = async (e) => {
     e.preventDefault();
     const city = e.target.elements.city.value;
-    //const country = e.target.elements.country.value;
 
     const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`);
     const data = await api_call.json(); 
