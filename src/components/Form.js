861b41aa65fa988a0760b035f95de0a1500956c3
Sarah <sarahahmad17@gmail.com>
2019-04-23 11:36:53 -0400

    css changes

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Form = props => (
     <form onSubmit={props.getWeather} >
-        <input type="text" name="city" placeholder="Enter city name here..." />
+        <input type="text" name="city" placeholder="Enter city..." />
         {/* <input type="text" name="country" placeholder="Country..."/> */}
         <button>Get Weather</button>
     </form>
--- src/components/Titles.js
+++ src/components/Titles.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Titles = () => (
     <div>
-        <h1>Weather Discovery</h1>            
-        <p>Find out the weather in any city across the world!</p>
+        <h1 className="title-container__title">Weather Discovery</h1>            
+        <p className="title-container__subtitle">Find out the weather in any city across the world!</p>
     </div>
 )
 
